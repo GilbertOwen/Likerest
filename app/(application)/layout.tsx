6f2b39e4928f0deb669389c6f069eae2ac619b2f
Gilbert Owen <moptius@gmail.com>
2024-06-16 20:01:47 +0700

    feat: created layout for public page

---
+++ app/(application)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./_components/Navbar";
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex pt-[80px] flex-col h-full w-full">
+      <Navbar />
+      {children}
+    </div>
+  );
+}
