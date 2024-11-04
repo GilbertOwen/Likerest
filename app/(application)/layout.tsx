ffd47c9ecba5a6ec9261d01b2eb9bd37f2faaf4a
Gilbert Owen <psychoganko@gmail.com>
2024-11-05 00:12:32 +0700

    style: adjusted the padding for the application

--- app/(application)/layout.tsx
+++ app/(application)/layout.tsx
@@ -6,7 +6,7 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex pt-[80px] flex-col h-full w-full">
+    <div className="flex pt-[70px]  flex-col h-full w-full">
       <Navbar />
       {children}
     </div>
