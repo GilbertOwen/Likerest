4bcacc3f90c3d97af5495da2b09c3ed1e890f9ca
Gilbert Owen <psychoganko@gmail.com>
2024-11-07 18:26:17 +0700

    feat: added analytics website vercel to the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className + ` overflow-x-hidden scroll-smooth relative m-0 p-0`}>{children}</body>
+      <body className={poppins.className + ` overflow-x-hidden scroll-smooth relative m-0 p-0`}>{children}
+        <Analytics/>
+      </body>
     </html>
   );
 }
