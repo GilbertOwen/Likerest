c2fecdffdf454b00df524b1f8f576f8aec85fd46
Gilbert Owen <88517849+GilbertOwen@users.noreply.github.com>
2024-12-10 12:21:56 +0700

    Applied authentication logic (#7)
    * feat: added analytics website vercel to the application
    
    * style: added more padding-top to public layout
    
    * style: changed all rectangle element border to rounded-md border
    
    * feat: added fade away to bottom of the image
    
    * style: changed the logo style
    
    * feat: Added more dependencies such as bcrypt, axios, zod, react icons
    
    * feat: Added toast container to root layout, so by doing it, we can use toast in the children file
    
    * feat: Uninstalled bcrypt and installed react-toastify
    
    * feat: Compressed the hero image so it will be more light
    
    * feat: Created the logic action to handle register user and return error response if there're any of it
    
    * feat: Added event when user is done with the register, direct the user to login component
    
    * feat: Integrate Register component with register user logic and added toast when it is completed

--- app/(application)/(authentication)/_components/Register.tsx
+++ app/(application)/(authentication)/_components/Register.tsx
@@ -1,28 +1,101 @@
-export default function RegisterAuth() {
+"use client";
+import { useFormState } from "react-dom";
+import { useEffect, useRef } from "react";
+import { toast } from "react-toastify";
+import registerUser from "../_actions/register-user";
+
+interface RegisterAuthProps {
+  changeState: () => void;
+}
+
+export default function RegisterAuth({ changeState }: RegisterAuthProps) {
+  const [state, formAction] = useFormState(registerUser, {
+    status: "",
+    message: "",
+    errors: {},
+  });
+
+  const registerForm: any = useRef(null);
+
+  useEffect(() => {
+    if (state.status === "success") {
+      toast.success(state.message, {
+        position: "top-left",
+      });
+      registerForm.current.reset();
+      changeState();
+    } else if (state.status === "failed") {
+      toast.error(state.message, {
+        position: "top-left",
+      });
+    }
+  }, [state]);
   return (
-    <form action="" className="flex flex-col gap-y-6">
+    <form
+      action={formAction}
+      ref={registerForm}
+      className="flex flex-col gap-y-6"
+    >
       <h1 className="text-3xl font-semibold border-b-2">REGISTER</h1>
       <div className="flex flex-col gap-y-4">
         <div className="flex flex-col gap-y-1">
           <label htmlFor="username" className="text-lg font-medium">
             Username
           </label>
-          <input type="text" id="username" className="rounded-md border-2 px-2 py-1.5" />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="rounded-md border-2 px-2 py-1.5"
+          />
+          {state.errors && state.errors.username ? (
+            <p className="text-xs text-[#FF4D4F] mt-1 font-medium">
+              {state.errors.username}
+            </p>
+          ) : (
+            <></>
+          )}
         </div>
         <div className="flex flex-col gap-y-1">
           <label htmlFor="email" className="rounded-md text-lg font-medium">
             Email
           </label>
-          <input type="email" id="email" className="rounded-md border-2 px-2 py-1.5" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="rounded-md border-2 px-2 py-1.5"
+          />
+          {state.errors && state.errors.email ? (
+            <p className="text-xs text-[#FF4D4F] mt-1 font-medium">
+              {state.errors.email}
+            </p>
+          ) : (
+            <></>
+          )}
         </div>
         <div className="flex flex-col gap-y-1">
           <label htmlFor="password" className="text-lg font-medium">
             Password
           </label>
-          <input type="password" id="password" className="rounded-md border-2 px-2 py-1.5" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="rounded-md border-2 px-2 py-1.5"
+          />
+          {state.errors && state.errors.password ? (
+            <p className="text-xs text-[#FF4D4F] mt-1 font-medium">
+              {state.errors.password}
+            </p>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
-      <button className="text-md rounded-md font-semibold px-4 py-2 hover:bg-white border-2 border-black box-border hover:text-black w-fit bg-black text-white">Register</button>
+      <button className="text-md rounded-md font-semibold px-4 py-2 hover:bg-white border-2 border-black box-border hover:text-black w-fit bg-black text-white">
+        Register
+      </button>
     </form>
   );
 }
