a4d1fc451964dec3c454d534f8ce50fa13e12bf7
Gilbert Owen <psychoganko@gmail.com>
2025-02-08 18:13:04 +0700

    feat: added authentication to layout public

--- app/(application)/layout.tsx
+++ app/(application)/layout.tsx
@@ -1,13 +1,20 @@
+"use server";
+import { cookies } from "next/headers";
 import Navbar from "./_components/Navbar";
 
-export default function PublicLayout({
+export default async function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  let cookie: any = await cookies();
+  let authToken: any= cookie.get('auth');
+  if (!cookie) {
+    authToken = false;
+  }
   return (
-    <div className="flex pt-[83px]  flex-col h-full w-full">
-      <Navbar />
+    <div className="flex pt-[92px]  flex-col h-full w-full">
+      <Navbar isAuthenticated={authToken ? true : false} />
       {children}
     </div>
   );
