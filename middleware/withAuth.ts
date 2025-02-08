f20a9ad31f3f6cac505caced340bd3e2cadb3191
Gilbert Owen <psychoganko@gmail.com>
2025-02-08 18:26:21 +0700

    feat: created authentication system to check every path or request that user's done

---
+++ middleware/withAuth.ts
@@ -0,0 +1,47 @@
+"use server";
+import axios from "axios";
+import { cookies } from "next/headers";
+import {
+  NextFetchEvent,
+  NextMiddleware,
+  NextRequest,
+  NextResponse,
+} from "next/server";
+import Cookies from "js-cookie";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+
+export default function withAuth(
+  middleware: NextMiddleware,
+  requireCheck: string[] = []
+) {
+  return async (req: NextRequest, next: NextFetchEvent) => {
+    const pathname = req.nextUrl.pathname;
+    const cookiess = await cookies();
+    const authToken: RequestCookie | undefined = cookiess.get("auth");
+    if (requireCheck.includes(pathname)) {
+      if (authToken) {
+        const token = await axios.post(
+          "http://localhost:3030/api/auth/me",
+
+          {},
+          {
+            headers: {
+              Authorization: authToken.value,
+            },
+          }
+        );
+        if (token.status === 401) {
+          const url = new URL(pathname, req.url);
+          cookiess.delete('auth');
+          url.searchParams.set("callbackUrl", encodeURI(req.url));
+          return NextResponse.redirect(url);
+        }
+      } else {
+        const url = new URL("/", req.url);
+        cookiess.delete('auth');
+        return NextResponse.redirect(url);
+      }
+    }
+    return middleware(req, next);
+  };
+}
