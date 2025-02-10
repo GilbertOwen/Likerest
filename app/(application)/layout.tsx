8f2e5a1f55f9783614fb5ea9d5e55c563a48a230
Gilbert Owen <psychoganko@gmail.com>
2025-02-10 15:04:29 +0700

    feat: added authentication on layout and propping to the navbar

--- app/(application)/layout.tsx
+++ app/(application)/layout.tsx
@@ -7,14 +7,36 @@ export default async function PublicLayout({
 }: {
   children: React.ReactNode;
 }) {
-  let cookie: any = await cookies();
-  let authToken: any= cookie.get('auth');
-  if (!cookie) {
-    authToken = false;
+  let cookieStore = await cookies();
+  let token = cookieStore.get("auth")?.value || null;
+  let authenticated = false;
+
+  console.log("Token:", token); // Debugging
+
+  if (token) {
+    const response = await fetch(
+      `${process.env.NEXT_INTERNAL_API_URL}/api/me`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `${token}`,
+        },
+        cache: "no-store",
+        credentials: "include",
+      }
+    );
+
+    if (response.status === 401) {
+      console.warn("Unauthorized! Removing auth cookie...");
+      await fetch(`${process.env.NEXT_INTERNAL_API_URL}/api/logout`, { method: "GET" }); // Remove the auth cookie
+    } else {
+      authenticated = response.ok;
+    }
   }
+
   return (
-    <div className="flex pt-[92px]  flex-col h-full w-full">
-      <Navbar isAuthenticated={authToken ? true : false} />
+    <div className="flex pt-[92px] flex-col h-full w-full">
+      <Navbar isAuthenticated={authenticated} />
       {children}
     </div>
   );
