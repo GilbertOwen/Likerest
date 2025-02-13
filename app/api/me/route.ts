d97bbf80e70c1a826aa2cf7a288a467c907197b6
Gilbert Owen <psychoganko@gmail.com>
2025-02-13 22:27:12 +0700

    feat: add route and process to check on backend logout route

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -8,16 +8,14 @@ export async function GET(req: NextRequest) {
   }
 
   try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_DOMAIN_NAME}/auth/me`,
-      {
-        headers: {
-          Authorization: authToken,
-        },
-      }
-    );
-    return NextResponse.json({ token: authToken }, {status : 200});
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN_NAME}/auth/me`, {
+      headers: {
+        Authorization: authToken,
+      },
+    });
+    return NextResponse.json({ ...response.data }, { status: 200 });
   } catch (error) {
+    await fetch(`${process.env.NEXT_INTERNAL_API_URL}/api/logout`);
     return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
   }
 }
